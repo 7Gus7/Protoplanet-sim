c25788085dbe5dc9ee0a8624173baa0006139f07
Angus Bews <angus.7@outlook.com>
2024-12-08 14:28:46 -0800

    results

--- results.py
+++ results.py
@@ -10,6 +10,7 @@ def main():
     time_taken_for_critical_mass_across_seeds = []
     sum_sizes = 0
     average_object_size_across_seeds_li = []
+    sample_var_across_runs_li = []
 
     max_num_entries = 0
     plots = []
@@ -27,8 +28,16 @@ def main():
                 nums = line.split(" ")
                 sum_sizes += int(nums[1]) #size
 
-            average_object_size = sum_sizes/num_entries
-            print(f"average object size for seed {seed} using p={probab} and gtype {g_type} = {average_object_size}")
+            average_object_size = sum_sizes/num_entries #for the run with a specfic seed
+            variance_sum = 0
+            for sample in lines:
+                difference = int(sample[1]) - average_object_size
+                variance_sum += difference**2
+
+            sample_var = 1/(len(num_entries) - 1) * variance_sum #sample variance of object size per run
+            sample_var_across_runs_li.append(sample_var)
+
+            print(f"average object size for seed {seed} using p={probab} and gtype {g_type} = {average_object_size}, with sample variance {sample_var}")
             average_object_size_across_seeds_li.append(average_object_size)
 
             avg_time_taken_for_critical_mass_across_seeds = sum(time_taken_for_critical_mass_across_seeds)/5
@@ -44,6 +53,7 @@ def main():
 
     averaged_list_of_sizes = [round(x/5, 3) for x in list_of_sizes]
     average_object_size_across_seeds_value = sum(average_object_size_across_seeds_li)/5 #divide by 5 for 5 random seeds
+    average_sample_var_across_runs_val = sum(sample_var_across_runs_li)/5
 
 
 
@@ -51,6 +61,7 @@ def main():
     print(f"printing results for gtpye {g_type} and probab {probab}:")
     print("average object size across the 5 seeds is: ", average_object_size_across_seeds_value)
     print("average time taken to reach critical mass across the 5 seeds is: ", avg_time_taken_for_critical_mass_across_seeds)
+    print(f"average sample variance across 5 runs is {average_sample_var_across_runs_val}")
 
     import matplotlib.pyplot as plt
     x = [i for i in range(len(list_of_sizes))]
