73ac048183b5d06d7d5f0d58eef93c817672b561
Khushboo Chugh <92592464+khushbooc8@users.noreply.github.com>
2024-12-07 23:43:56 -0800

    final sim code

--- sim.py
+++ sim.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-
 import random as rand
+import math
 
 class Object:
     def __init__(self, size = 0, particles_dict = {}):
@@ -17,20 +16,20 @@ def scale_down(n, scaling_factor):
 
     
 #inputs
-G2_low = 0.8
-G2_sun = 1
-G2_high = 1.04
-high_dens_vel = 0.3 # need to confirm
-low_dens_vel = 0.5 # need to confirm
+G_low = 0.8
+G_sun = 1
+G_high = 1.04
+high_dens_vel = 0.7 # need to confirm
+low_dens_vel = 0.3 # need to confirm
 
 #current iterations inputs
-m = G2_sun
+m = G_sun
 p = low_dens_vel #probability of collisions
-p_ie = 0.3 # probability of inelastic collisions, adjust to realism if possible
+p_ie = 0.5 # probability of inelastic collisions, adjust to realism if possible
 
 #inner variables
 n = convert_to_particles(m)  # number of particles
-n = scale_down(n, scaling_factor=1e-50)
+n = scale_down(n, scaling_factor=1e-53) # should be 1e-52
 critical_mass_in_particles = 0.08 * n # should be some % of n
 total_particles = n + (n * 0.01) # 99% of the cloud is gas and 1% is dust
 num_collisions = 0
@@ -38,39 +37,42 @@ num_inelastic_collisions = 0
 Clock = 0
 largest_object_size = 0
 objects_dict = {}
-
-rand.seed(10)
+seed = 10
+rand.seed(seed)
 
 def main_simulation():
     
     event = initialize()
-
+    print("n is ", n, " and total num particles is ", total_particles, " and critical mass is ", critical_mass_in_particles)
     #run the simulation until the largest object in the system reaches the critical mass of the sun
     while largest_object_size < critical_mass_in_particles:
-        print(largest_object_size)
         process_collision(event)
         # create the next event
         event = create_event()
         
     print_stats()
-        
+
+def increment_clock():
+    global Clock
+    Clock +=  -1 * math.log(rand.random()) / p   
+
 
 def initialize():
-    global Clock, num_collisions, num_inelastic_collisions
+    global num_collisions, num_inelastic_collisions
     particle1 = rand.randint(1,n)
     while True:
         particle2 = rand.randint(1,n)
         #make sure the particles picked are not the same
         if particle2 != particle1:
-            Clock += 1
             if rand.random() >= p: #a collision occurs
+                #increment time between collisions
+                increment_clock()
                 num_collisions += 1
                 if rand.random() >= p_ie: #inelastic collision occurs
                     num_inelastic_collisions += 1
                     #return a tuple of the collided particles
                     return (particle1, particle2)
                
- 
 
 def process_collision(event):
     global objects_dict, largest_object_size
@@ -112,16 +114,17 @@ def process_collision(event):
 
 
 def create_event():
-    global Clock, num_collisions, num_inelastic_collisions
+    global num_collisions, num_inelastic_collisions
     particle1 = rand.randint(1,n)
     object_num = find_object(particle1) 
     while True:
         particle2 = rand.randint(1,n)
         #make sure the particles picked are not the same, and that particle2 has not already collided with particle1
         if particle2 != particle1 and particle2 not in objects_dict[object_num].particles_dict: 
-            Clock += 1
             if rand.random() >= p: #a collision occurs
                 num_collisions += 1
+                #increment time between collisions
+                increment_clock()
                 if rand.random() >= p_ie: #inelastic collision occurs
                     num_inelastic_collisions += 1
                     #return a tuple of the collided particles
@@ -153,10 +156,36 @@ def find_object(particle):
     
 
 def print_stats():
+    scaling_fac = 1e-53 #scaling factor should be 1e-52
+    print("Scaling factor: ", scaling_fac)
     print("Number of collisions: ", num_collisions)
     print("Number of inelastic collisions: ", num_inelastic_collisions)
-    print("Time taken to reach critical mass: ", Clock)
+    print("Time taken to reach critical mass (Clock): ", Clock)
+    
     print("Largest object size: ", largest_object_size)
+    
+    output_data_li = []
+    with open(f"output_for_{m},{p},{seed}.txt", "w") as f:
+        for obj in objects_dict:
+            output_data_li.append((obj, objects_dict[obj].size))
+            
+        f.write("Number of collisions: " + str(num_collisions) + "\n")
+        f.write("Number of inelastic collisions: " + str(num_inelastic_collisions) + "\n")
+        f.write("Time taken to reach critical mass: " + str(Clock) + "\n")
+        f.write("Largest object size: " + str(largest_object_size) + "\n")
+    
+        # sort output_data_li based on size of object, from largest to smallest
+        output_data_li.sort(key = lambda x: x[1], reverse = True)
+        for sorted_obj in output_data_li:
+            f.write(str(sorted_obj[0]) + " " + str(sorted_obj[1]) + "\n")
+    
+    #plot the data where the x axis is the object number and the y axis is the size of the object
+    import matplotlib.pyplot as plt
+    x = [i for i in range(len(output_data_li))]
+    y = [i[1] for i in output_data_li]
+    plt.scatter(x, y)
+    plt.xlabel("Object Number")
+    plt.ylabel("Object Size")
 
 if __name__ == "__main__":
     main_simulation()
